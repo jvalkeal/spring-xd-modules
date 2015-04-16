922d2117b6e308d279e439123542475919801a1e
Janne Valkealahti <janne.valkealahti@gmail.com>
2015-04-16 14:41:02 +0100

    updates

--- gpfdist/src/main/java/org/springframework/xd/greenplum/gpfdist/GPFDistCodec.java
+++ gpfdist/src/main/java/org/springframework/xd/greenplum/gpfdist/GPFDistCodec.java
@@ -18,9 +18,6 @@ package org.springframework.xd.greenplum.gpfdist;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import reactor.fn.Consumer;
 import reactor.fn.Function;
 import reactor.io.buffer.Buffer;
@@ -28,20 +25,13 @@ import reactor.io.codec.Codec;
 
 public class GPFDistCodec extends Codec<Buffer, Buffer, Buffer> {
 
-	private final static Log log = LogFactory.getLog(GPFDistCodec.class);
-
 	final byte[] h1 = Character.toString('D').getBytes(Charset.forName("UTF-8"));
-
-	final Object lock = new Object();
 
 	@SuppressWarnings("resource")
 	@Override
 	public Buffer apply(Buffer t) {
-//		log.info("DDDD " + t.hashCode());
-//		synchronized (lock) {
 			byte[] h2 = ByteBuffer.allocate(4).putInt(t.flip().remaining()).array();
 			return new Buffer().append(h1).append(h2).append(t).flip();
-//		}
 	}
 
 	@Override
